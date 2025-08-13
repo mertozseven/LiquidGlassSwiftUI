601e812dd91ff2d5fa2ec90e2bc531d1270b100b
Mert Adem Özseven <developer@mertozseven.com>
2025-08-13 22:29:16 +0300

    Add: Implemented Liquid Glass UI

--- LiquidGlassExample/App/LiquidGlassExampleApp.swift
+++ LiquidGlassExample/App/LiquidGlassExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LiquidGlassExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView(quote: "Liquid Glass Example")
         }
     }
 }
